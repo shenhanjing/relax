82e3818328b6e2f51ec8f1b92b48985a8ff674a2
shenhanjing <shenhanjing@outlook.com>
2024-08-02 22:43:54 +0800

    ENH: dynamic_to_static: support Add/Sub/FloorDiv in dynamic_to_static pass
    Signed-off-by: shenhanjing <shenhanjing@outlook.com>

--- src/relax/transform/replace_dynamic_with_static.cc
+++ src/relax/transform/replace_dynamic_with_static.cc
@@ -25,10 +25,13 @@ class DynamicToStaticReplacer : public ExprMutator {
   explicit DynamicToStaticReplacer(IRModule ctx_module) : ExprMutator(ctx_module) {}
 
   PrimExpr Var2IntImm(const tir::Var& var) {
-    static std::map<std::string, int32_t> shape_value = {{"n", 128}, {"m", 128}};
+    static std::map<std::string, int32_t> shape_value = {{"seq_len", 16}};
     std::string value_name = var.get()->name_hint;
     auto it = shape_value.find(value_name);
-    ICHECK(it != shape_value.end()) << "Var " << var << "is not defined in variable map.";
+    if (it == shape_value.end()) {
+      std::cout << "[WARNING]: Var \"" << var << "\" is not defined in variable map." << std::endl;
+      return var;
+    }
     int32_t num = it->second;
     PrimExpr int_imm = tir::make_const(DataType::Int(64), num);
     return int_imm;
@@ -40,10 +43,34 @@ class DynamicToStaticReplacer : public ExprMutator {
       return Var2IntImm(var);
     } else if (expr->IsInstance<tir::IntImmNode>()) {
       return expr;
+    } else if (expr->IsInstance<tir::AddNode>()) {
+      tir::Add op = Downcast<tir::Add>(expr);
+      PrimExpr a = PrimExpr2Static(op->a);
+      PrimExpr b = PrimExpr2Static(op->b);
+      const IntImmNode* const_a = a.as<IntImmNode>();
+      const IntImmNode* const_b = b.as<IntImmNode>();
+      if (const_a && const_b) {
+        int64_t value = const_a->value + const_b->value;
+        PrimExpr int_imm = tir::make_const(DataType::Int(64), value);
+        return int_imm;
+      }
+      return tir::Add(a, b);
+    } else if (expr->IsInstance<tir::SubNode>()) {
+      tir::Sub op = Downcast<tir::Sub>(expr);
+      PrimExpr a = PrimExpr2Static(op->a);
+      PrimExpr b = PrimExpr2Static(op->b);
+      const IntImmNode* const_a = a.as<IntImmNode>();
+      const IntImmNode* const_b = b.as<IntImmNode>();
+      if (const_a && const_b) {
+        int64_t value = const_a->value - const_b->value;
+        PrimExpr int_imm = tir::make_const(DataType::Int(64), value);
+        return int_imm;
+      }
+      return tir::Sub(a, b);
     } else if (expr->IsInstance<tir::MulNode>()) {
-      tir::Mul mul = Downcast<tir::Mul>(expr);
-      PrimExpr a = PrimExpr2Static(mul->a);
-      PrimExpr b = PrimExpr2Static(mul->b);
+      tir::Mul op = Downcast<tir::Mul>(expr);
+      PrimExpr a = PrimExpr2Static(op->a);
+      PrimExpr b = PrimExpr2Static(op->b);
       const IntImmNode* const_a = a.as<IntImmNode>();
       const IntImmNode* const_b = b.as<IntImmNode>();
       if (const_a && const_b) {
@@ -52,8 +79,25 @@ class DynamicToStaticReplacer : public ExprMutator {
         return int_imm;
       }
       return tir::Mul(a, b);
+    } else if (expr->IsInstance<tir::FloorDivNode>()) {
+      tir::FloorDiv op = Downcast<tir::FloorDiv>(expr);
+      PrimExpr a = PrimExpr2Static(op->a);
+      PrimExpr b = PrimExpr2Static(op->b);
+      const IntImmNode* const_a = a.as<IntImmNode>();
+      const IntImmNode* const_b = b.as<IntImmNode>();
+      if (const_a && const_b) {
+        int64_t a_value = const_a->value;
+        int64_t b_value = const_b->value;
+        int value = a_value / b_value;
+        if ((a_value % b_value != 0) && ((a_value < 0) != (b_value < 0))) {
+          value--;
+        }
+        PrimExpr int_imm = tir::make_const(DataType::Int(64), value);
+        return int_imm;
+      }
+      return tir::FloorDiv(a, b);
     } else {
-      LOG(FATAL) << "Not Implemented";
+      LOG(FATAL) << expr->GetTypeKey() << " is not implemented.";
       throw;
     }
   }
